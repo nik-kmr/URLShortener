2bc9209c410cb0ec8fbe1b8bae9267f87e4b142d
Nikhil Kumar <nikkmr@amazon.com>
2025-06-22 23:39:30 +0530

    adding DataBaseModule via env variables

---
+++ src/module/DataBaseModule.java
@@ -0,0 +1,22 @@
+package module;
+
+import com.google.inject.AbstractModule;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+
+public class DataBaseModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(DataSource.class).toInstance(createHikariDataSource());
+    }
+
+    private DataSource createHikariDataSource() {
+        final HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(System.getenv("JDBC_URL"));
+        config.setUsername(System.getenv("DB_USERNAME"));
+        config.setPassword(System.getenv("DB_PASSWORD"));
+        return new HikariDataSource(config);
+    }
+}
